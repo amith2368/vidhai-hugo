735314dec95ad942bc2fa8c06702e62ccae89925
Amith <amith2368@gmail.com>
2020-12-23 16:13:37 +0530

    Chnages in static page

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,4 +1,21 @@
 import $ from 'jquery'
 import 'bootstrap'
+
 // import 'bootstrap/js/src/carousel'
 // import 'bootstrap/js/src/util'
+
+$(document).ready(function() {
+
+    $('.number').each(function () {
+    $(this).prop('Counter',0).animate({
+    Counter: $(this).text()
+    }, {
+    duration: 4000,
+    easing: 'swing',
+    step: function (now) {
+    $(this).text(Math.ceil(now));
+    }
+    });
+    });
+    
+    });
